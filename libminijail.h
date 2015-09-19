13010947484bd0c099f9c8f4703bc436130bc5ce
Dylan Reid <dgreid@chromium.org>
2015-09-18 17:36:20 -0700

    minijail: Add an option to drop caps without setting securebits
    This option can be used if there is a desire to not set securebits but
    still drop capabilities. The new -x option will be used to restrict
    functionality in a container such as CAP_BLOCK_SUSPEND, without losing
    other capabilities.
    
    This also allows insecure caps to be dropped for a static binary because
    the inheritance rules are less strict without the securebits set.
    
    With '-x' root is still a privileged user, with '-c' root doesn't
    default to having all privileges.  The -x option is not secure but it
    works well when using capabilities to restrict functionality for policy
    instead of security.
    
    Change-Id: I1af8ebf916a807bed35e91ef5ddb3d7cf2264ecd
    Signed-off-by: Dylan Reid <dgreid@chromium.org>

--- libminijail.h
+++ libminijail.h
@@ -48,6 +48,7 @@ void minijail_use_seccomp_filter(struct minijail *j);
 void minijail_parse_seccomp_filters(struct minijail *j, const char *path);
 void minijail_log_seccomp_filter_failures(struct minijail *j);
 void minijail_use_caps(struct minijail *j, uint64_t capmask);
+void minijail_use_caps_insecure(struct minijail *j, uint64_t capmask);
 void minijail_namespace_vfs(struct minijail *j);
 void minijail_namespace_enter_vfs(struct minijail *j, const char *ns_path);
 void minijail_namespace_net(struct minijail *j);
