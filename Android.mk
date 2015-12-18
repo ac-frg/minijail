b01b916e9b72ba5e1621e5c2143030e973bf17a4
Jeff Vander Stoep <jeffv@google.com>
2015-12-18 13:19:53 -0800

    Pass clang CFLAGS into generate scripts
    Without this change x86_64 system calls are incorrectly added
    to libsyscalls.c on x86 builds. This passes in -D__i386__
    for x86 and -D__x86_64__ for x86_64 targets.
    
    Change-Id: I036565f65ce8a26554fe2a9f00a4d22834abea81

--- Android.mk
+++ Android.mk
@@ -36,24 +36,28 @@ LOCAL_MODULE := libminijail_generated
 LOCAL_MODULE_CLASS := STATIC_LIBRARIES
 generated_sources_dir := $(local-generated-sources-dir)
 
-$(generated_sources_dir)/libsyscalls.c: PRIVATE_CUSTOM_TOOL = $< "$(lastword $(CLANG)) -isystem bionic/libc/kernel/uapi/asm-$(TARGET_ARCH)" $@
+$(generated_sources_dir)/libsyscalls.c: PRIVATE_CUSTOM_TOOL = $< "$(lastword $(CLANG)) \
+	$(RS_TRIPLE_CFLAGS) -isystem bionic/libc/kernel/uapi/asm-$(TARGET_ARCH)" $@
 $(generated_sources_dir)/libsyscalls.c: $(LOCAL_PATH)/gen_syscalls.sh
 	$(transform-generated-source)
 LOCAL_GENERATED_SOURCES_$(TARGET_ARCH) += $(generated_sources_dir)/libsyscalls.c
 
-$(generated_sources_dir)/libconstants.c: PRIVATE_CUSTOM_TOOL = $< "$(lastword $(CLANG)) -isystem bionic/libc/kernel/uapi/asm-$(TARGET_ARCH)" $@
+$(generated_sources_dir)/libconstants.c: PRIVATE_CUSTOM_TOOL = $< "$(lastword $(CLANG)) \
+	$(RS_TRIPLE_CFLAGS) -isystem bionic/libc/kernel/uapi/asm-$(TARGET_ARCH)" $@
 $(generated_sources_dir)/libconstants.c: $(LOCAL_PATH)/gen_constants.sh
 	$(transform-generated-source)
 LOCAL_GENERATED_SOURCES_$(TARGET_ARCH) += $(generated_sources_dir)/libconstants.c
 
 # For processes running in 32-bit compat mode on 64-bit processors.
 ifdef TARGET_2ND_ARCH
-$(generated_sources_dir)/libsyscalls.c: PRIVATE_CUSTOM_TOOL = $< "$(lastword $(CLANG)) -isystem bionic/libc/kernel/uapi/asm-$(TARGET_2ND_ARCH)" $@
+$(generated_sources_dir)/libsyscalls.c: PRIVATE_CUSTOM_TOOL = $< "$(lastword $(CLANG)) \
+	$(RS_TRIPLE_CFLAGS) -isystem bionic/libc/kernel/uapi/asm-$(TARGET_2ND_ARCH)" $@
 $(generated_sources_dir)/libsyscalls.c: $(LOCAL_PATH)/gen_syscalls.sh
 	$(transform-generated-source)
 LOCAL_GENERATED_SOURCES_$(TARGET_2ND_ARCH) += $(generated_sources_dir)/libsyscalls.c
 
-$(generated_sources_dir)/libconstants.c: PRIVATE_CUSTOM_TOOL = $< "$(lastword $(CLANG)) -isystem bionic/libc/kernel/uapi/asm-$(TARGET_2ND_ARCH)" $@
+$(generated_sources_dir)/libconstants.c: PRIVATE_CUSTOM_TOOL = $< "$(lastword $(CLANG)) \
+	$(RS_TRIPLE_CFLAGS) -isystem bionic/libc/kernel/uapi/asm-$(TARGET_2ND_ARCH)" $@
 $(generated_sources_dir)/libconstants.c: $(LOCAL_PATH)/gen_constants.sh
 	$(transform-generated-source)
 LOCAL_GENERATED_SOURCES_$(TARGET_2ND_ARCH) += $(generated_sources_dir)/libconstants.c
