0f630ecd344b0aa8eccd4edf6af6540650d2723e
Matthew Dempsky <mdempsky@google.com>
2016-02-11 11:55:10 -0800

    defer checking ELF linkage type until after entering VFS namespace
    This ensures that when we check the target program's ELF linkage, it
    corresponds to the program we actually intend to execute.
    
    Bonus: The check for libminijailpreload.so can now check that it's
    accessible inside the VFS namespace too, and the access(filename,
    X_OK) check tests the program's execute permissions according to our
    final user credentials rather than the early credentials.
    
    TEST=Use minijail0 -V to run a program in a pivot_root'd mount namespace
    
    Bug: 26947503
    Change-Id: Id9466731d63cf85283d8f38714831ce737e2b25d

--- libminijail.h
+++ libminijail.h
@@ -164,8 +164,7 @@ int minijail_run(struct minijail *j, const char *filename,
 		 char *const argv[]);
 
 /*
- * Run the specified command in the given minijail, execve(2)-style.
- * Used with static binaries, or on systems without support for LD_PRELOAD.
+ * Deprecated alias for minijail_run.
  */
 int minijail_run_no_preload(struct minijail *j, const char *filename,
 			    char *const argv[]);
@@ -200,15 +199,7 @@ int minijail_run_pid_pipes(struct minijail *j, const char *filename,
 			   int *pstdin_fd, int *pstdout_fd, int *pstderr_fd);
 
 /*
- * Run the specified command in the given minijail, execve(2)-style.
- * Update |*pchild_pid| with the pid of the child.
- * Update |*pstdin_fd| with a fd that allows writing to the child's
- * standard input.
- * Update |*pstdout_fd| with a fd that allows reading from the child's
- * standard output.
- * Update |*pstderr_fd| with a fd that allows reading from the child's
- * standard error.
- * Used with static binaries, or on systems without support for LD_PRELOAD.
+ * Deprecated alias for minijail_run_pid_pipes.
  */
 int minijail_run_pid_pipes_no_preload(struct minijail *j, const char *filename,
 				      char *const argv[], pid_t *pchild_pid,
