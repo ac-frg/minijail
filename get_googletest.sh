823cfa8311296aeddc3eeb0da250ef433302a602
Jorge Lucangeli Obes <jorgelo@google.com>
2017-01-26 17:21:16 -0500

    Port Minijail unit tests to Google Test.
    Google Test is designed to be included directly with the project using
    it. Add a simple shell script to get Google Test sources.
    
    Test: Builds on Android, desktop Linux.
    Change-Id: Iabc63fa1cb847b668645dbf5468454f025c93721

---
+++ get_googletest.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+wget -q -nc --secure-protocol=TLSv1 https://github.com/google/googletest/archive/release-1.8.0.tar.gz -O googletest-release-1.8.0.tar.gz
+tar zxvf googletest-release-1.8.0.tar.gz
