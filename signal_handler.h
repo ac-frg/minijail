3f6520809fdd8514a2831dff99f8ccc8d9ea9a0b
Jorge Lucangeli Obes <jorgelo@google.com>
2017-05-18 15:57:53 -0400

    Add SIGUNUSED for MIPS.
    Bug: 38424765
    Test: Build aosp_mips-eng.
    Change-Id: I5c29be9b3a5aecdeb55f5cac3715ca7736189be2

--- signal_handler.h
+++ signal_handler.h
@@ -9,6 +9,17 @@
 #ifndef SIGNAL_HANDLER_H
 #define SIGNAL_HANDLER_H
 
+/*
+ * SIGUNUSED for MIPS. Other architectures define SIGUNUSED as the last valid
+ * signal (by number), so do the same for MIPS.
+ * See http://elixir.free-electrons.com/linux/v4.4.68/source/arch/mips/include/uapi/asm/signal.h#L57
+ */
+#if defined(__mips__)
+#if !defined(SIGUNUSED)
+#define SIGUNUSED 31
+#endif
+#endif
+
 int install_sigsys_handler();
 
 #endif /* SIGNAL_HANDLER_H */
