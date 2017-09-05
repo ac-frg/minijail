2ebf820b5d80b96f26953845f284049b5549a6c7
Luis Hector Chavez <lhchavez@google.com>
2017-09-05 09:17:22 -0700

    Improve the way uid/gid changes in unprivileged userns
    This change uses whatever was passed into the -u/-g flags as the user to change
    in the user namespace. This is used to fix an issue where calling open(2) on a
    file on the tmpfs created by minijail would return EOVERFLOW[1]. An easy way to
    reproduce is running this on a 4.8 kernel (or Ubuntu Xenial, which has this
    change backported):
    
      $ ./minijail0 -T static -Ut -- /bin/bash -c 'touch /tmp/foo'
    
    This change allows a non-zero uid/gid to be mapped to the current user when
    entering a namespace, to avoid the above issue.
    
    1: More information about the bug here:
    https://bugs.launchpad.net/ubuntu/+source/linux/+bug/1659087
    
    Bug: None
    Test: make tests
    Test: ./minijail0 -T static -Ut -u 1000 -g 1000 -M -m -- \
          /bin/bash -c 'touch /tmp/foo'
    Change-Id: I393daaf8c2b2355e33c75a908345bb03f1980271

--- system.h
+++ system.h
@@ -68,6 +68,9 @@ int write_proc_file(const struct logger *logger, pid_t pid, const char *content,
 int setup_mount_destination(const char *source, const char *dest, uid_t uid,
 			    uid_t gid, bool bind);
 
+int lookup_user(const char *user, uid_t *uid, gid_t *gid);
+int lookup_group(const char *group, gid_t *gid);
+
 #ifdef __cplusplus
 }; /* extern "C" */
 #endif
