7b7ef83f7b9787560625b06f29a5bee8533c683d
Stephen Barber <smbarber@google.com>
2019-12-09 17:20:28 -0800

    Build minijail as rust crate
    minijail isn't packaged for normal Linux systems. Package it as a crate so it
    can be built into the crosvm binary when minijail isn't available via
    pkg-config.
    
    BUG=chromium:1032360
    TEST=cargo build
    
    Change-Id: I7939d02399b28fe55fcd53fddec246e6129b4181

---
+++ lib.rs
@@ -0,0 +1,16 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// libminijail bindings for Rust.
+
+// Generated with `bindgen --default-enum-style rust --whitelist-function '^minijail_.*' \
+// --whitelist-var '^MINIJAIL_.*' --no-layout-tests --output libminijail.rs libminijail.h -- \
+// -DUSE_BINDGEN -D_FILE_OFFSET_BITS=64`.
+// Manually fix up rlim64_t to be u64.
+//
+// Enum variants in rust are customarily camel case, but bindgen will leave the original names
+// intact.
+#[allow(non_camel_case_types)]
+mod libminijail;
+pub use crate::libminijail::*;
