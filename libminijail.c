3ce84a1700883bf6f37781ad8ade7ecb1b5d2dcb
Allen Webb <allenwebb@google.com>
2021-07-16 15:39:29 -0500

    Clear FD_CLOEXEC for preserved file descriptors.
    Bug: 186443800
    Test: cros_workon_make --board=${BOARD} --test chromeos-base/minijail
    Change-Id: Ia6ed2ec00d3e574b519b75c6bec2fffc9d2b10bd

--- libminijail.c
+++ libminijail.c
@@ -2691,6 +2691,25 @@ static int redirect_fds(struct minijail *j)
 	for (size_t i = 0; i < j->preserved_fd_count; i++) {
 		if (j->preserved_fds[i].parent_fd ==
 		    j->preserved_fds[i].child_fd) {
+			// Clear CLOEXEC if it is set so the FD will be
+			// inherited by the child.
+			int flags =
+			    fcntl(j->preserved_fds[i].child_fd, F_GETFD);
+			if (flags == -1) {
+				pwarn("failed to get flags for %d",
+				      j->preserved_fds[i].parent_fd);
+				continue;
+			}
+			if ((flags & FD_CLOEXEC) == 0) {
+				continue;
+			}
+
+			flags &=~FD_CLOEXEC;
+			fcntl(j->preserved_fds[i].child_fd, F_SETFD, flags);
+			if (flags == -1) {
+				pwarn("failed to clear CLOEXEC for %d",
+				      j->preserved_fds[i].parent_fd);
+			}
 			continue;
 		}
 		if (dup2(j->preserved_fds[i].parent_fd,
