e89714219b1b0892726b941aa6d5f5b8d7b09158
Zi Lin <lziest@google.com>
2021-09-10 04:11:02 +0000

    minijail: Add a config parser to load conf file in cli.
    1. It supports '#' for comment.
    2. Each conf line should be of format 'key=value'.
    3. Blank lines are ignored.
    
    Bug: 199189187
    Test: unit tests
    
    Change-Id: I68768d55b93e7edf66ce3ffa49cac351f7a5ac30

---
+++ config_parser.h
@@ -0,0 +1,37 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef CONFIG_PARSER_H
+#define CONFIG_PARSER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct config_entry {
+	const char *key;
+	const char *value;
+	struct config_entry *next;
+	struct config_entry *last;
+	/* the size of the entry list, assuming the current entry
+	 * is the header, always counting from the next entry.
+	 */
+	size_t list_len;
+};
+
+struct config_entry *new_config_entry(void);
+
+/* Free the list pointed to by |head|. |head| itself will not be freed.
+ */
+void free_config_entry_list(struct config_entry *head);
+
+struct config_entry *parse_config_line(const char* config_line);
+int parse_config_file(FILE *config_file, struct config_entry *head);
+
+#ifdef __cplusplus
+}; /* extern "C" */
+#endif
+
+#endif /* CONFIG_PARSER_H */
